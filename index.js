bee7b54f80371bcd044d7b08e9f3971a802becc3
aytc <aytacalir@gmail.com>
2024-08-06 21:13:11 +0300

    kaydet

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 
+let date = document.getElementById("tarih");
+let miktar = document.getElementById("miktar");
+let harcamaalani = document.getElementById("harcamaalani");
+let table = document.querySelector(".table")
+
+document.getElementById("kaydet").addEventListener("click",()=>{
+    let tarih1 = date.value ;
+    let miktar1 = miktar.value;
+    let harcama1 =harcamaalani.value;
+
+    console.log(tarih1,miktar1, harcama1);
+
+    const tr= document.createElement('tr');
+    const newElement = `<td>${tarih1}</td>
+                   <td>${miktar1}</td>
+                   <td>${harcama1}</td>
+                   <td>${miktar1}</td>`
+
+     tr.innerHTML=newElement ; 
+     table.appendChild(tr);
+                
+})
+
+
+
+
+
+
+
