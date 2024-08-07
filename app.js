55755d1062bc0999b877c4331419bab0cf75db04
aytc <aytacalir@gmail.com>
2024-08-07 20:24:48 +0300

    app js intro

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let date = document.getElementById("tarih");
+let miktar = document.getElementById("miktar");
+let harcamaalani = document.getElementById("harcamaalani");
+let table = document.querySelector(".table")
+let ekle = document.querySelector('.ekle')
+let gelir = document.querySelector('#gelir')
+let total = 0;
+let gelir1 = gelir.value;
+let temizle = document.querySelector('.temizle')
